76dc5ccb7ccd184ed4b08d8d2bb7d1b13d698782
Mylan <developerspanly@icloud.com>
2023-05-29 17:04:30 +0200

    Arrangement du joueur

--- Assets/Mylan/Scripts/GameManager.cs
+++ Assets/Mylan/Scripts/GameManager.cs
@@ -7,11 +7,12 @@ using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    public float RunTimer = 20f;
+    public float RunTimer = 10f;
     public Transform NativeDuckPosition;
     public GameObject PlayerObject;
     public int CurrentIndexOfRun = 1;
     private Teleportation teleportation;
+
     public void Start()
     {
         teleportation = FindObjectOfType<Teleportation>();
@@ -24,13 +25,16 @@ public class GameManager : MonoBehaviour
     IEnumerator RunTimerClock()
     {
         yield return new WaitForSeconds(RunTimer);
+        PlayerObject.SetActive(false);
         CurrentIndexOfRun++;
-        teleportation.TeleportSystem();
+        //teleportation.TeleportSystem();
+        PlayerObject.transform.position = NativeDuckPosition.position;
         Debug.Log("New run in coming " + CurrentIndexOfRun);
         StartCoroutine(RunTimerClock());
         if(CurrentIndexOfRun >= 3)
         {
             print("Game has finished!");
         }
+        PlayerObject.SetActive(true);
     }
 }
