cf62f0e061d00d23a687da3cb5f09d692b2b8295
Mylan <developerspanly@icloud.com>
2023-05-30 20:39:45 +0200

    Rangement de la hiérarchie

--- Assets/Mylan/Scripts/GameManager.cs
+++ Assets/Mylan/Scripts/GameManager.cs
@@ -7,34 +7,42 @@ using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    /*public float RunTimer = 10f;
+    public float RunTimer = 10f;
     public Transform NativeDuckPosition;
-    public GameObject PlayerObject;
+    public GameObject PlayerObject, testObject;
     public int CurrentIndexOfRun = 1;
     private Teleportation teleportation;
+
+    public bool canWalk = true;
 
     public void Start()
     {
         teleportation = FindObjectOfType<Teleportation>();
 
         Debug.Log("Game has started!");
-        teleportation.TeleportSystem();
         StartCoroutine(RunTimerClock());
     }
 
     IEnumerator RunTimerClock()
     {
         yield return new WaitForSeconds(RunTimer);
-        PlayerObject.SetActive(false);
         CurrentIndexOfRun++;
-        //teleportation.TeleportSystem();
-        PlayerObject.transform.position = NativeDuckPosition.position;
-        Debug.Log("New run in coming " + CurrentIndexOfRun);
+        teleportation.TeleportSystem();
+        Debug.Log("New run incoming: " + CurrentIndexOfRun);
         StartCoroutine(RunTimerClock());
         if(CurrentIndexOfRun >= 3)
         {
             print("Game has finished!");
         }
-        PlayerObject.SetActive(true);
-    }*/
+    }
+
+    private KinematicCharacterController.KinematicCharacterMotor kinematicMotor;
+
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+            teleportation.TeleportSystem();
+        }
+    }
 }
--- Assets/Teleportation.cs
+++ Assets/Teleportation.cs
@@ -4,14 +4,50 @@ using UnityEngine;
 
 public class Teleportation : MonoBehaviour
 {
-    /*private GameManager gameManager;
+    private GameManager gameManager;
+    private KinematicCharacterController.KinematicCharacterMotor kinematicMotor;
 
     public void Start()
     {
         gameManager = FindObjectOfType<GameManager>();
+        kinematicMotor = transform.GetComponent<KinematicCharacterController.KinematicCharacterMotor>();
+        print("Bien instancié");
     }
     public void TeleportSystem()
     {
-        transform.position = gameManager.NativeDuckPosition.position;
-    }*/
+        StartCoroutine(tpSys());
+    }
+    IEnumerator tpSys()
+    {
+        DisableAllScripts();
+        Vector3 teleportPosition = gameManager.NativeDuckPosition.position;
+        kinematicMotor.MoveCharacter(teleportPosition);
+        yield return new WaitForSeconds(0.5f);
+        EnableAddScripts();
+        gameManager.canWalk = true;
+    }
+    private void DisableAllScripts()
+    {
+        MonoBehaviour[] scripts = GetComponents<MonoBehaviour>();
+
+        foreach (MonoBehaviour script in scripts)
+        {
+            if (script != this) // Vérifie si le script est différent du script Teleportation lui-même
+            {
+                script.enabled = false; // Désactive le script
+            }
+        }
+    }
+    private void EnableAddScripts()
+    {
+        MonoBehaviour[] scripts = GetComponents<MonoBehaviour>();
+
+        foreach (MonoBehaviour script in scripts)
+        {
+            if (script != this) // Vérifie si le script est différent du script Teleportation lui-même
+            {
+                script.enabled = true; // Désactive le script
+            }
+        }
+    }
 }
